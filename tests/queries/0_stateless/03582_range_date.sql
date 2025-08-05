23485ca8452a50296094c568206a78b5b7a14892
Mark Andreev <mark.andreev@gmail.com>
2025-08-06 00:06:13 +0100

    Add interval days

--- tests/queries/0_stateless/03582_range_date.sql
+++ tests/queries/0_stateless/03582_range_date.sql
@@ -1,5 +1,6 @@
 -- range function to support Date, DateTime, Date32 and DateTime64:
 select range(toDate('2000-01-01'), toDate('2000-01-05'))
+select range(toDate('2000-01-01'), toDate('2000-01-05'), interval 2 days)
 select range(toDate32('2000-01-01'), toDate32('2000-01-05'))
 select range(toDateTime('2020-01-01 00:00:01'), toDateTime('2020-01-01 00:00:05'))
 select range(toDateTime('2020-01-01 00:00:01'), toDateTime('2020-01-01 00:00:10'), interval 2 seconds)
